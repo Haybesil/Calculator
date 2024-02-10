992d946c0e1f3dd2b2988dc683d526b337ab1dad
Silas Abayomi <abayomisilas50@gmail.com>
2024-02-10 20:06:00 +0100

    updated html, js and css

--- index.js
+++ index.js
@@ -13,6 +13,35 @@ function Display(value) {
   }
 
 
-  function DisplayValue(value) {
-    document.getElementById('display').value =+ value;
+
+  // Dark and Light mode 
+
+  function darkMode() { 
+
+    let body = document.body;
+    let cal = document.querySelector(".calculator");
+    body.style.backgroundColor = "black";
+    cal.style.backgroundColor = "white";
+
+    const nodeList = document.querySelectorAll(".button1");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.backgroundColor = "white";
+  nodeList[i].style.color = "black";
+ }
+    
   }
+
+  function lightMode() {
+    
+    let body = document.body;
+    let cal = document.querySelector(".calculator");
+    body.style.backgroundColor = "white";
+    cal.style.backgroundColor = "black";
+
+    const nodeList = document.querySelectorAll(".button1");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.backgroundColor = "black";
+  nodeList[i].style.color = "white";
+  }
+}
+ 
